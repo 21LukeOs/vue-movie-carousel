255078402d1d77e7d9f8ddb4c24d8eacb1f12d49
Luke Osgood <lukemosgood@gmail.com>
2021-02-16 21:15:37 -0500

    created carousel component and added logo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Movie Carousel"/>
+    <Carousel msg="Movie Carousel"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Carousel from './components/Carousel.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Carousel
   }
 }
 </script>
